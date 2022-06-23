50a5e03b43364a78ff2b3a5467b1374848a483e4
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-06-23 14:02:14 -0400

    write simple code to pass Employee method tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -7,3 +7,27 @@ test('creates an employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test("gets employee's name as an object", () => {
+    const employee = new Employee('Alexus');
+
+    expect(employee.getName()).toHaveProperty('name')
+});
+
+test("gets employee's id as an object", () => {
+    const employee = new Employee('Alexus');
+
+    expect(employee.getId()).toHaveProperty('id')
+});
+
+test("gets employee's email as an object", () => {
+    const employee = new Employee('Alexus');
+
+    expect(employee.getEmail()).toHaveProperty('email')
+});
+
+test("gets employee's role as an object", () => {
+    const employee = new Employee('Alexus');
+
+    expect(employee.getRole()).toHaveProperty('role','employee');
+});
